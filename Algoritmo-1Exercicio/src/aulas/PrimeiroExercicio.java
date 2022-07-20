9c88ccdfb05668d505f176063503544109291be9
Weslei Lima <exmo.weslei@gmail.com>
2022-07-20 14:36:00 -0300

    primeiro commit

---
+++ Algoritmo-1Exercicio/src/aulas/PrimeiroExercicio.java
@@ -0,0 +1,29 @@
+package aulas;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+
+		// Primeiro exercício
+		/*
+		 * int numero1 = 15; double numero2 = 20.5;
+		 * 
+		 * String nome = "WESLEI"; char sexo = 'M';
+		 * 
+		 * Boolean verdadeiro = false; Boolean falso = true;
+		 * 
+		 * System.out.println("Número1 " + numero1 + " Número2 " + numero2);
+		 * System.out.println(nome + " " + sexo); System.out.println(verdadeiro);
+		 * System.out.println(falso);
+		 */
+
+		// Segundo exercício
+
+		String nome = "Weslei";
+		int idade = 49;
+		double dinheiro = 50000.5;
+		
+		System.out.println(nome + " tem " + idade + " anos, e tem R$ " + dinheiro + " reais");
+	}
+
+}
